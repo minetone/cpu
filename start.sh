4bf6e93bbe686a08af8cfc8722ffb9c08bdaa8d5
minetone <80713959+minetone@users.noreply.github.com>
2021-03-16 01:54:35 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qhz83yzczprh077exmzy60yzq9gkz6qhje3gwqp.test1
 
 
 done
